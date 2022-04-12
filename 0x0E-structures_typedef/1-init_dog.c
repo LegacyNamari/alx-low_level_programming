c4e56b92e548f56da205d05098e466b64b81b12e
LegacyNamari <nellywangari30@gmail.com>
2022-04-12 19:13:21 +0300

    Typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+/**
+ * init_dog -  initialize a variable of type struct dog
+ * @d: puntero a la estructrua dog
+ * @name: puntero a la varaible name de la estructura dog
+ * @age: edad
+ * @owner: puntero a la variable owner de la estructrua dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog;
+}
